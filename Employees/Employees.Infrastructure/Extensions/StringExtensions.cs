2722c9044197072959b8fb1830de1d8f80d6f593
rokoruzic <roko.ruzic4@gmail.com>
2019-01-04 18:47:33 +0100

    added few string extensions

---
+++ Employees/Employees.Infrastructure/Extensions/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Employees.Infrastructure.Extensions
+{
+	public static class StringExtensions
+	{
+		public static string CapitalizeFirstLetterAndRemoveMultipleWhiteSpaces(this string text)
+		{
+			if (text.Length == 0)
+				return text;
+			text = text.Trim();
+			var regex = new Regex(@"\s{2,}");
+			while (regex.IsMatch(text))
+				text = regex.Replace(text, " ", 1);
+			text = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text);
+			return text;
+		}
+
+		public static string RemoveAllWhiteSpaces(this string text)
+		{
+			text = text.Trim();
+			text = text.Replace(" ","");
+			return text;
+		}
+	}
+}
--- Employees/Program.cs
+++ Employees/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Employees.Infrastructure.Extensions;
 
 namespace Employees
 {
@@ -17,6 +18,7 @@ namespace Employees
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new Form1());
+			var macka = "idiot";
 		}
 	}
 }
